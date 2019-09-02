e4c918faa8bb8bd1cfb193d00e51e6b85f9606e5
Luis <lugaliguori@gmail.com>
2019-09-02 16:41:54 -0400

    adding google services files

--- www/js/controllers/tabsController.js
+++ www/js/controllers/tabsController.js
@@ -7,5 +7,25 @@
         tabsController.$inject = ['$scope','$localStorage'];
         function tabsController($scope,$localStorage){
             $scope.id = $localStorage.uid
-        }        
+        }
+        
+        FCMPlugin.getToken(function(token) {
+            //this is the fcm token which can be used
+            //to send notification to specific device 
+            console.log(token);
+            //FCMPlugin.onNotification( onNotificationCallback(data), successCallback(msg), errorCallback(err) )
+            //Here you define your application behaviour based on the notification data.
+            FCMPlugin.onNotification(function(data) {
+                console.log(data);
+                //data.wasTapped == true means in Background :  Notification was received on device tray and tapped by the user.
+                //data.wasTapped == false means in foreground :  Notification was received in foreground. Maybe the user needs to be notified.
+                // if (data.wasTapped) {
+                //     //Notification was received on device tray and tapped by the user.
+                //     alert(JSON.stringify(data));
+                // } else {
+                //     //Notification was received in foreground. Maybe the user needs to be notified.
+                //     alert(JSON.stringify(data));
+                // }
+            });
+        });
 })()  
