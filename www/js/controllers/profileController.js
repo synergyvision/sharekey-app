8cee792228ab256afb9c0e3a7c450537ef84bbc5
Luis <lugaliguori@gmail.com>
2019-09-03 11:20:22 -0400

    converting base64 to file

--- www/js/controllers/profileController.js
+++ www/js/controllers/profileController.js
@@ -58,6 +58,20 @@
               })
             }
 
+            var dataURItoBlob = function (dataURI) {
+              var byteString = atob(dataURI.toString().split(',')[1]);
+          
+              //var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+          
+              var ab = new ArrayBuffer(byteString.length);
+              var ia = new Uint8Array(ab);
+              for (var i = 0; i < byteString.length; i++) {
+                  ia[i] = byteString.charCodeAt(i);
+              }
+              var blob = new Blob([ab], {type: 'image/png'}); //or mimeString if you want
+              return blob;
+          }
+
             $scope.takePicture = function() {
               var options = {
                   quality : 75,
@@ -73,8 +87,9 @@
               };
               $ionicPlatform.ready(function (){
                 $cordovaCamera.getPicture(options).then(function(imageData) {
-                    var imgURI = atob(imageData);
-                    console.log(imgURI)
+                    var imgURI = "data:image/jpeg;base64," + imageData;
+                    var blob = dataURItoBlob(imgURI);
+                    var image = new File([blob], 'image.png');
                     $scope.uploadPhoto(imgURI)
                 }, function(err) {
                     alert(err)
